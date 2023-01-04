32607af26e2385e8042cd94d950d989f5f85eaf0
pai-plznw4me <plznw4me@publicai.co.kr>
2023-01-04 20:46:48 +0900

    공제 및 환급 기능 추가

--- parser.py
+++ parser.py
@@ -1,5 +1,4 @@
 """
-
 상시 근로 인정 예외 대상
     1. 임원 여부(제외),
     2. 1년 미만 계약직(제외)
@@ -308,8 +307,8 @@ def first_deduction(young_counts, etc_counts):
     Description:
         최초 공제 정보를 계산해 반환합니다.
 
-    :param ndarray young_counts: 청년 근로자, 값 순서는 연도순으로 나열되어 있어야 합니다.
-    :param ndarray etc_counts: 기타 근로자
+    :param ndarray young_counts: 연도별 청년 근로자, 값 순서는 연도순으로 나열되어 있어야 합니다.
+    :param ndarray etc_counts: 연도별 기타 근로자
     :return:
         [[index, 청년 공제 증가, 기타 공제 증가],
          [index, 청년 공제 증가, 기타 공제 증가]]
@@ -322,35 +321,184 @@ def first_deduction(young_counts, etc_counts):
     # 최초 공제 시기
     first_deduction_index = np.where(wkr_diff > 0)[0]
 
-    # 최초 공제 세부사항
+    # 최초 공제 시기에 청년/기타 작년도 대비 근무 개월 수 차이
     target_young_deductions = yng_diff[first_deduction_index]  # 청년
     target_etc_deductions = etc_diff[first_deduction_index]  # 기타
 
-    # 세부 사항에서 rate 가 음수이면 0으로 치환합니다.
-    target_young_deductions = np.where(target_young_deductions <= 0, 0, target_young_deductions)
-    target_etc_deductions = np.where(target_etc_deductions <= 0, 0, target_etc_deductions)
-
-    # axis=0 최초 공제 연도, axis=1 (index, 청년 공제 비율, 기타 공제 비율)
+    # axis=0 최초 공제 연도, axis=1 (index, 작년도 대비 청년 근무 개월 수 차이, 작년도 대비 기타 근무 개월 수 차이)
     first_deduction_infos = np.stack([first_deduction_index, target_young_deductions, target_etc_deductions], axis=-1)
     return first_deduction_infos
 
 
-def add_deduction(young_counts, etc_counts, index):
+def deduction_mask(young_counts, etc_counts, index):
     """
     Description:
-        추가 공제 자격여부를 확인 한 후 여부를 확인합니다.
-    :return:
+        최초 공제 시기 이후 2년동안 청년 공제, 기타 공제 자격여부를 파악해 반환합니다.
+        공제 규칙은 wiki 을 참조하세요.
+
+    Args:
+    :param ndarray young_counts: 연도별 청년 근무 달(month) 수
+    :param ndarray etc_counts: 연도별 기타 근무 달(month) 수
+    :param int index: 최초 공제를 받은 연도 인덱스
+
+    :ndarray return:
+        +--+------+------+------+------+------+
+        |  | 2019 | 2020 | 2021 | 2022 | 2023 |
+        +--+------+------+------+------+------+
+        |  | NaN  |  NaN | True | True | -1  |
+        +--+------+------+------+------+------+
+        |  | NaN  |  NaN | True | False| -1  |
+        +--+------+------+------+------+------+
+    NaN 은 공제 계산을 하지 않은것
     """
-    n = len(young_counts)
+    year_length = len(young_counts)
+    deduction_year = 2  # 추가 공제 년도
+
+    # axis 0 에 0th index => 청년 공제, axis 0 에 1th index => 기타 공제
+    deduction_calendar = np.zeros(shape=(len(young_counts), 2)) + np.nan
+
     yng_diff = get_diff(young_counts)
     etc_diff = get_diff(etc_counts)
     wkr_diff = yng_diff + etc_diff
 
-    sliced_wrk = wkr_diff[index:]
-    sliced_yng = yng_diff[index:]
-    sliced_etc = etc_diff[index:]
+    # 최초 공제시 근로 개월수 작녀 대비 차이
+    wrk_diff_std = wkr_diff[index]
+    yng_diff_std = yng_diff[index]
+    etc_diff_std = etc_diff[index]
 
-    return False
+    for i in np.arange(index, np.minimum(index + deduction_year + 1, year_length)):
+        # 공제 자격 여부를 검토합니다.
+        # 공제 자격을 유지 합니다.
+        # 청년 공제 가능 여부를 검토 합니다.
+
+        if wrk_diff_std <= wkr_diff[i]:
+            # 청년 공제 가능시 deduction_calendar 에 청년 공제 비율을 기록합니다.
+            if yng_diff_std <= yng_diff[i]:
+                deduction_calendar[i, 0] = True
+            else:
+                deduction_calendar[i, 0] = False
+                # 기타 공제 가능시 deduction_calendar 에 기타 공제 비율을 기록합니다.
+
+            if etc_diff_std <= etc_diff[i]:
+                deduction_calendar[i, 1] = True
+            else:
+                deduction_calendar[i, 1] = False
+        # 상시 근로자가 한명이라도 2년안에 줄면 공제 자격을 상실 합니다.
+        else:
+            deduction_calendar[i, 0] = -1
+            deduction_calendar[i, 1] = -1
+            break
+    return deduction_calendar
+
+
+def deduction_table(capital_area):
+    """
+    공제 금액 테이블을 반환합니다.
+    (⚠️ 중소 기업 이하만 적용 가능하다. 매년 년도별 공제 금액을 업데이트 해야 한다.)
+    또한 수도권 / 비수도권이 나눠어져 있다.
+    공제율(중소 기업 이하):
+        2018~2020, 2023년
+            +----------+------------+--------+
+            |          | 수도권 밖  | 수도권 |
+            +----------+------------+--------+
+            | 청년     | 1200       | 1100   |
+            +----------+------------+--------+
+            | 청년 외  | 770        | 700    |
+            +----------+------------+--------+
+        2021,2022
+            +----------+------------+--------+
+            |          | 수도권 밖  | 수도권 |
+            +----------+------------+--------+
+            | 청년     | 1300       | 1100   |
+            +----------+------------+--------+
+            | 청년 외  | 770        | 700    |
+            +----------+------------+--------+
+    :param capital_area:
+    :return:
+    """
+    capital_df = pd.DataFrame()
+    capital_df.index = ['young', 'etc']
+    capital_df[2018] = [1100, 700]
+    capital_df[2019] = [1100, 700]
+    capital_df[2020] = [1100, 700]
+    capital_df[2021] = [1100, 700]
+    capital_df[2022] = [1100, 700]
+    capital_df[2023] = [1100, 700]
+
+    noncptl_df = pd.DataFrame()
+    noncptl_df.index = ['young', 'etc']
+    noncptl_df[2018] = [1200, 770]
+    noncptl_df[2019] = [1200, 770]
+    noncptl_df[2020] = [1200, 770]
+    noncptl_df[2021] = [1300, 770]
+    noncptl_df[2022] = [1300, 770]
+    noncptl_df[2023] = [1200, 770]
+
+    if capital_area:
+        tax_table = capital_df
+    else:
+        tax_table = noncptl_df
+
+    return tax_table
+
+
+def deduction_tax(year, type, capital_area):
+    """
+    해당년도의 청년 또는 기타 공제 금액을 산정해 반환한다.
+    (⚠️ 중소 기업 이하만 적용 가능하다. 매년 년도별 공제 금액을 업데이트 해야 한다.)
+
+    Args:
+        :param int year: 적용 연도
+        :param str type: 'young' or 'etc'
+        :param bool capital_area: 수도권 여부, 수도권이면 True
+
+        :int return: 공제 금액
+    """
+    assert (type == 'young') or (type == 'etc'), 'type 값으로는 "young" , "etc" 만 가능합니다.'
+
+    tax_table_df = deduction_table(capital_area)
+    tax = tax_table_df.loc[type, year]
+    return tax
+
+
+def calculate_tax(year, capital_area, yng_diff, etc_diff):
+    """
+    공제 받을 세금을 계산해 반환
+    공제 금액은 최대 상시 근로자 수에 비례한다.
+
+    :param int year: 적용 연도
+    param bool capital_area: 수도권 여부, 수도권이면 True
+    :param ndarray young_counts: 연도별 청년 근로자, 값 순서는 연도순으로 나열되어 있어야 합니다.
+    :param ndarray etc_counts: 연도별 기타 근로자
+
+    :return:
+    """
+    # 청년/기타 근로 개월 수 차이
+    total_diff = yng_diff + etc_diff
+
+    # 해당 연도 및 지역의 청년/기타 공제 금액
+    young_tax = deduction_tax(year, 'young', capital_area)
+    etc_tax = deduction_tax(year, 'etc', capital_area)
+
+    if yng_diff >= 0 and etc_diff >= 0:
+        young_diff_tax = young_tax * yng_diff
+        etc_diff_tax = etc_tax * etc_diff
+
+    elif yng_diff <= 0 and etc_diff >= 0:
+        young_diff_tax = 0
+        etc_diff_tax = etc_tax * total_diff
+
+    elif yng_diff >= 0 and etc_diff <= 0:
+        young_diff_tax = young_tax * total_diff
+        etc_diff_tax = 0
+
+    elif yng_diff <= 0 and etc_diff <= 0:
+        young_diff_tax = 0
+        etc_diff_tax = 0
+    else:
+        raise NotImplementedError
+
+    return young_diff_tax, etc_diff_tax
 
 
 def intersection(start_date_1, end_date_1, start_date_2, end_date_2, dummy_date):
@@ -397,6 +545,76 @@ def intersection(start_date_1, end_date_1, start_date_2, end_date_2, dummy_date)
     return start_date, end_date
 
 
+def get_deduction(deductions, year):
+    """
+    Description:
+        해당 년도에 받은 총 공제 금액을 제공 한다.
+
+    Args:
+        :param list deductions:
+            [deduction, deduction ... deduction]
+            deduction:
+                +--+------+------+------+------+------+
+                |       | 2019 | 2020 | 2021 | 2022 | 2023 |
+                +--+------+------+------+------+------+
+                | young | NaN  |  NaN | True | True | NaN  |
+                +--+------+------+------+------+------+
+                | etc   | NaN  |  NaN | True | False| NaN  |
+                +--+------+------+------+------+------+
+        :param int year:
+
+    :return:
+    """
+    tax = []
+    for deduction_df in deductions:
+
+        mask = deduction_df < 0
+        deduction_df[mask] = 0
+        deduction_df = deduction_df.fillna(0)
+        tax.append(deduction_df.loc[:, year].sum())
+    total_tax = np.sum(tax)
+    return total_tax
+
+
+def get_refund(deductions, year):
+    """
+    Description:
+        환급해야 할 세금 금액을 계산해 반환합니다.
+        :param list deductions:
+            [deduction, deduction ... deduction]
+            deduction:
+                +--+------+------+------+------+------+
+                |       | 2019 | 2020 | 2021 | 2022 | 2023 |
+                +--+------+------+------+------+------+
+                | young | NaN  |  NaN | True | True | NaN  |
+                +--+------+------+------+------+------+
+                | etc   | NaN  |  NaN | True | False| NaN  |
+                +--+------+------+------+------+------+
+        :param int year:
+    :return:
+    """
+
+    # 환급 받아야 할 공제을 추출합니다.
+    refund_index = []
+    refund_taxs = []
+    for ind, deduction_df in enumerate(deductions):
+        # 추가 징수
+        if deduction_df.loc['young', year] == -1:  # 'young' 이 -1 이면 'etc' 도 반드시 -1 입니다.
+            deduction_df = deduction_df.replace({-1: 0})
+            deduction_df = deduction_df.fillna(0)
+            refund_index.append(ind)
+            # 환급 해야 할 돈을 계산합니다. 받았던 모든 공제를 반납해야 합니다.
+            tax = deduction_df.sum()
+            refund_taxs.append(tax)
+        else:
+            refund_taxs.append(0)
+
+    # 환급 해야 할 돈을 반환합니다.
+    refund_tax = np.sum(refund_taxs)
+
+    return refund_tax
+
+
 if __name__ == '__main__':
     path = './data/사업장가입자명부_20221222 (상실자포함).xls'
 
@@ -405,11 +623,40 @@ if __name__ == '__main__':
     end_date = '2022-12-31'
     curr_date = pd.Timestamp.today()
     calendar = generate_work_calendar(path, start_date, end_date, curr_date=curr_date)
+    capital_area = True
 
     # 각 년도별 상시 근무자 인원 및 청년 근무자 인원
     calendar_sum = calendar.iloc[:, :].sum(axis=0)
     calendar_sum.iloc[0] = '합계'
 
-    # 공제 금액 계산
-    first_deduction_infos = first_deduction(calendar_sum[1:1 + 5].values, calendar_sum[1 + 5:1 + 5 + 5].values)
-    pass
+    # 청년 근로 및 기타 근로자 수를 계산합니다.
+    n_workers = calendar_sum[1:1 + 5].values
+    n_youngs = calendar_sum[1 + 5:1 + 5 + 5].values
+    n_etc = n_workers - n_youngs
+
+    # 각 년도별 공제 표 생성
+    deductions = []
+    first_deduction_infos = first_deduction(n_youngs, n_etc)
+    years = [2018, 2019, 2020, 2021, 2022]
+    for info in first_deduction_infos:
+        index = info[0]
+        yng_diff = info[1]
+        etc_diff = info[2]
+        mask = deduction_mask(n_youngs, n_etc, index)
+        deduction_df = pd.DataFrame(mask.T, columns=years, index=['young', 'etc'])
+        #
+        young_tax, etc_tax = calculate_tax(year=years[index],
+                                           capital_area=capital_area,
+                                           yng_diff=yng_diff,
+                                           etc_diff=etc_diff)
+        #
+        deduction_df.iloc[:, :] = np.array([[young_tax], [etc_tax]]) * deduction_df.values
+        deductions.append(deduction_df)
+
+    # 총합 공제 금액 계산
+    deduction_tax = get_deduction(deductions, 2022)
+
+    # 공제 금액 반납
+    refund_tax = get_refund(deductions, 2022)
+
+    print('2022년 공제 받은 금액 : {} \n2022년 추가 납부 금액 : {}'.format(deduction_tax, refund_tax))
